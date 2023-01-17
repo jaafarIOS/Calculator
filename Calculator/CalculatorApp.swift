d52c4fc08f91b9ecc43e47c047bc4a3f64bb0cc4
Jzubaidi <jaafarwerdi87@gmail.com>
2023-01-17 14:54:00 -0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Consultant on 1/17/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
