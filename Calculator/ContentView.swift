e7819edfff2896b4b57187093732689d9f9488a9
Jzubaidi <jaafarwerdi87@gmail.com>
2023-01-17 15:26:10 -0500

    Calculator in SwiftUI

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -8,15 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+// the Result
+    @State var finalValue:String = "DigitalCurry Recipe"
+// User insert
+    @State var calExpression: [String] = []
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack {
+                Text(self.finalValue)
+                    .font(Font.custom("HelveticaNeue", size: 50))
+                    .frame(idealWidth: 100, maxWidth: .infinity, idealHeight: 100,maxHeight: .infinity, alignment: .center)
+                    .foregroundColor(Color.white)
+                Text(flattenTheExpression(exps: calExpression))
+                                    .font(Font.custom("HelveticaNeue-Thin", size: 24))
+                                    .frame(alignment: Alignment.bottomTrailing)
+                                    .foregroundColor(Color.white)
+                
+                Spacer()
+                
+                
+            }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0,maxHeight: .infinity, alignment: .leading)
+                .background(.blue)
+            VStack {
+            }
+            .background(Color.black)
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, idealHeight: 414, maxHeight: .infinity, alignment: .leading)
         }
-        .padding()
+        .background(Color.black)
+        .edgesIgnoringSafeArea(.all)
     }
+}
+
+func flattenTheExpression(exps: [String]) -> String{
+    var calExp = ""
+    for exp in exps {
+        calExp.append(exp)
+    }
+    return calExp
 }
 
 struct ContentView_Previews: PreviewProvider {
